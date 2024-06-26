87ed43aaa60d3b6aae3870310b2a42eb28ea085a
Joe7381 <167350930+Joe7381@users.noreply.github.com>
2024-06-26 20:34:15 +0200

    Update HelperFunctions.py

--- object_tracker_package/object_tracker_package/HelperFunctions.py
+++ object_tracker_package/object_tracker_package/HelperFunctions.py
@@ -1,6 +1,6 @@
 # Helper Functions
 def pixel_to_cm(pixel, marker_size):
-    return pixel * (5.2 / marker_size)  # Marker eig. 5,2
+    return (pixel / 40)  # Marker eig. 2.01, 5.2 / marker_size
 
 #def cm_to_pixel(cm, marker_size):
     return cm / (6.4 * marker_size)
