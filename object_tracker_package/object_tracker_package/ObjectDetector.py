7134e3b7b3d8e635f6c387dc2ba350d5121b2fc4
Joe7381 <167350930+Joe7381@users.noreply.github.com>
2024-06-26 20:34:43 +0200

    Update ObjectDetector.py

--- object_tracker_package/object_tracker_package/ObjectDetector.py
+++ object_tracker_package/object_tracker_package/ObjectDetector.py
@@ -7,6 +7,12 @@ class ObjectDetector:
         self.model = YOLO(model_path).to(device)
       
     def detect_objects(self, frame):
+        # Konvertiert Frame in Graustufen
+        #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Anwenden von Otsu's Schwellenwertmethode
+        #_, otsu_frame = cv2.threshold(gray_frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # Optional: Konvertieren Sie das binarisierte Bild zurück zu einem 3-Kanal-Bild, falls nötig
+        #otsu_frame_3 = cv2.cvtColor(otsu_frame, cv2.COLOR_GRAY2BGR)
         results = self.model(frame)
         if results and results[0].boxes:
             return results[0].boxes.data.tolist()
@@ -21,8 +27,6 @@ class ObjectDetector:
                
                 conf = result[4]
                 cls = int(result[5])
-                label = 'Einhorn' if cls == 1 else 'Katze'
+                label = 'unicorn' if cls == 1 else 'cat'
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-      
-   
